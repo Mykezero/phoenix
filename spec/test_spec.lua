57461c8d9bae8dea9bc40eb670ca978aa59351ae
Mykezero <Mykezero@users.noreply.github.com>
2019-05-05 12:52:34 -0400

    Start building test for recording method calls

--- spec/test_spec.lua
+++ spec/test_spec.lua
@@ -2,7 +2,7 @@ require 'busted.runner'()
 
 describe("Engage", function()
     it("should send '/attack on' command", function()
-        local sut = creatsut()
+        local sut = createsut()
         stub(sut, "sendcommand")
         sut.engage();
         assert.stub(sut.sendcommand).was.called()
@@ -12,7 +12,7 @@ end)
 
 describe("Run", function()
     it("should execute behavior", function ()
-        local sut = creatsut()
+        local sut = createsut()
         sut.run({ execute = function() sut.status = 1 end})
         assert.are.equal(1, sut.status)
     end)
@@ -20,7 +20,7 @@ end)
 
 describe("FindMob", function ()
     it("should find the closest mob", function()
-        local sut = creatsut()
+        local sut = createsut()
         local context = { entities = {
             {  name = "sheep",  distance = 15.0 },
             {  name = "bee",  distance = 17.0 }
@@ -31,8 +31,8 @@ describe("FindMob", function ()
 end)
 
 describe("Replay", function()
-	it("should replay the given logs", function()
-		local sut = creatsut();
+	it("should execute method with string argument", function()
+		local sut = createsut()
 		stub(sut, "sendcommand")		
 		sut.replay({ 
 			method = "sendcommand", 
@@ -40,6 +40,16 @@ describe("Replay", function()
 			}
 		)
 		assert.stub(sut.sendcommand).was.called_with("/attack on")
+	end)
+end)
+
+describe("Record", function()
+	it("record method call with one argument", function() 
+		local sut = createsut()
+		sut.sendcommand("/attack on")
+		local result = sut.getlogs()
+		assert.are.equal("sendcommand", result.method)
+		assert.are.equal("/attack on", result.inputs)
 	end)
 end)
 
@@ -60,6 +70,6 @@ function findlog()
 end
 
 
-function creatsut()
+function createsut()
     return require("phoenix")
 end
