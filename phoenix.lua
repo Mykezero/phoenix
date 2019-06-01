a203ea19bf7645ff60fce811c0f932b5c10eaed5
Mykezero <Mykezero@users.noreply.github.com>
2019-06-01 00:51:31 -0400

    Move addon metadata to top of file

--- phoenix.lua
+++ phoenix.lua
@@ -1,9 +1,9 @@
-require "common"
-require "core"
-
 _addon.author   = 'Mykezero'
 _addon.name     = 'phoenix'
 _addon.version  = '1.0.0'
+
+require "common"
+require "core"
 
 ashita.register_event('load', function()
     phoenix.load()
